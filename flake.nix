e1e64ca42a4c7ff1b25b0d9b4e58ae8ced1d2f14
drzzln <drzzln@protonmail.com>
2026-03-15 21:10:05 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
   description = "Akeyless custom producer examples - webhook-based custom secret producers in Go and Python";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go python3 python3Packages.pip docker ];
-      };
-    });
+
+  outputs = inputs:
+    (import "${inputs.substrate}/lib/repo-flake.nix" {
+      inherit (inputs) nixpkgs flake-utils;
+    }) {
+      self = inputs.self;
+      language = "shell";
+      description = "Akeyless custom producer examples - webhook-based custom secret producers in Go and Python";
+    };
 }
